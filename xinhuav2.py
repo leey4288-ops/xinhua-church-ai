3c277d31b7a7669a054e21675c8dee72307838a2
leey4288-ops <leey4288@gmail.com>
2026-02-28 14:12:30 +0800

    Add files via upload

--- xinhuav2.py
+++ xinhuav2.py
@@ -3,13 +3,6 @@ import time
 import streamlit as st
 
 st.write(st.secrets["OPENAI_API_KEY"][:10])  # 只顯示前 10 個字
-# ==============================
-# 頁面設定
-# ==============================
-st.set_page_config(
-    page_title="新化教會 AI 同工",
-    page_icon="logo.png"
-)
 
 # ==============================
 # 置中 Logo
@@ -18,7 +11,7 @@ col1, col2, col3 = st.columns([1,2,1])
 with col2:
     st.image("logo.png", width=150)
 
-st.title("新化教會 AI 同工")
+    st.title("新化教會 AI 同工")
 
 # ==============================
 # 初始化 OpenAI 客戶端
